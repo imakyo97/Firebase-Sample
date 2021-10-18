2fee76acb0661418c556d116ed862961ed646770
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-10-18 22:50:32 +0900

    LoginViewControllerを実装

--- FirebaseUI-Sample/FirebaseUI-Sample/LoginViewController.swift
+++ FirebaseUI-Sample/FirebaseUI-Sample/LoginViewController.swift
@@ -6,7 +6,56 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 final class LoginViewController: UIViewController {
 
+    enum Mode {
+        case login
+        case register // 登録
+    }
+
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var userTextField: UITextField!
+    @IBOutlet private weak var mailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var enterButton: UIButton!
+
+    private let mode: Mode
+
+    // MARK: - init
+    init?(coder: NSCoder, mode: Mode) {
+        self.mode = mode
+        super.init(coder: coder)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    private func setupMode() {
+        switch mode {
+        case .login:
+            userNameLabel.isHidden = true
+            userTextField.isHidden = true
+            enterButton.titleLabel?.text = "ログイン"
+        case .register:
+            enterButton.titleLabel?.text = "登録"
+        }
+    }
+
+    @IBAction private func didTapCancelBarButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction private func didTapSaveButton(_ sender: Any) {
+    }
+
+    @IBAction private func didTapEnterButton(_ sender: Any) {
+    }
 }
