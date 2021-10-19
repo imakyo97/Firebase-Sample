23f712ec3729928e14e60e8a3f704ebf6e23fbb9
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-10-19 13:16:38 +0900

    FirebaseUI-Sample1InitialCommit

---
+++ FirebaseUI-Sample1/FirebaseUI-Sample1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseUI-Sample1
+//
+//  Created by 今村京平 on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
