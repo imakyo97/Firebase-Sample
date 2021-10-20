619c184456ed942b542788392100f7d2e0a3b39e
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-10-20 15:49:43 +0900

    FirebaseAuth-SampleのメインビューをTableViewで実装

--- FirebaseAuth-Sample/FirebaseAuth-Sample/ViewController.swift
+++ FirebaseAuth-Sample/FirebaseAuth-Sample/ViewController.swift
@@ -8,60 +8,86 @@
 import UIKit
 import FirebaseAuth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet private weak var userImageView: UIImageView!
-    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var settingTableView: UITableView!
 
-    private var handle: AuthStateDidChangeListenerHandle?
+//    private var handle: AuthStateDidChangeListenerHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "設定"
+        setupTableView()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // 認証状態をリッスン
-        // ログイン状態が変わるたびに呼ばれる
-        handle = Auth.auth().addStateDidChangeListener { auth, user in
-            print("user.uid: \(user?.uid)")
-            print("user.displayName: \(user?.displayName)")
-            if let displayName = user?.displayName {
-                print("💣")
-                self.userNameLabel.text = displayName
-            } else {
-                self.userNameLabel.text = "ログアウト中"
-            }
-        }
+    private func setupTableView() {
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        // リスナーをデタッチ
-        Auth.auth().removeStateDidChangeListener(handle!)
+    private let items: [String] = [
+        "アカウント"
+    ]
+
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
     }
 
-    @IBAction private func didTapLoginButton(_ sender: Any) {
-        let loginViewController = LoginViewController.instantiate(mode: .login)
-        let navigationController = UINavigationController(rootViewController: loginViewController)
-        navigationController.modalPresentationStyle = .fullScreen
-        present(navigationController, animated: true, completion: nil)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
     }
 
-    @IBAction private func didTapSignUpButton(_ sender: Any) {
-        let loginViewController = LoginViewController.instantiate(mode: .create)
-        let navigationController = UINavigationController(rootViewController: loginViewController)
-        navigationController.modalPresentationStyle = .fullScreen
-        present(navigationController, animated: true, completion: nil)
+    // MARK: - UIViewControllerDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 画面遷移を実装
     }
-
-    @IBAction func didTapLogoutButton(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-        }
-    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        // 認証状態をリッスン
+//        // ログイン状態が変わるたびに呼ばれる
+//        handle = Auth.auth().addStateDidChangeListener { auth, user in
+//            print("user.uid: \(user?.uid)")
+//            print("user.displayName: \(user?.displayName)")
+//            if let displayName = user?.displayName {
+//                print("💣")
+//                self.userNameLabel.text = displayName
+//            } else {
+//                self.userNameLabel.text = "ログアウト中"
+//            }
+//        }
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        // リスナーをデタッチ
+//        Auth.auth().removeStateDidChangeListener(handle!)
+//    }
+//
+//    @IBAction private func didTapLoginButton(_ sender: Any) {
+//        let loginViewController = LoginViewController.instantiate(mode: .login)
+//        let navigationController = UINavigationController(rootViewController: loginViewController)
+//        navigationController.modalPresentationStyle = .fullScreen
+//        present(navigationController, animated: true, completion: nil)
+//    }
+//
+//    @IBAction private func didTapSignUpButton(_ sender: Any) {
+//        let loginViewController = LoginViewController.instantiate(mode: .create)
+//        let navigationController = UINavigationController(rootViewController: loginViewController)
+//        navigationController.modalPresentationStyle = .fullScreen
+//        present(navigationController, animated: true, completion: nil)
+//    }
+//
+//    @IBAction func didTapLogoutButton(_ sender: Any) {
+//        let firebaseAuth = Auth.auth()
+//        do {
+//            try firebaseAuth.signOut()
+//        } catch let signOutError as NSError {
+//            print("Error signing out: %@", signOutError)
+//        }
+//    }
 }
 
